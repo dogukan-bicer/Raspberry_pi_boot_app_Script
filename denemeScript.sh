4658ce767cd25b4733b27bab06c372f57b8c669c
Doğukan Biçer <dogukanbicer52@gmail.com>
2022-08-30 21:36:43 +0300

    Add files via upload

---
+++ denemeScript.sh
@@ -0,0 +1,9 @@
+#!/home/dogukan/Desktop
+#chmod +x denemeScript.sh
+echo "Script calisti!!!!"
+cd /home/dogukan/Desktop
+mkdir YeniDosya
+cd /home/dogukan/Desktop/YeniDosya
+touch dosya.txt
+open dosya.txt
+#echo "Dosya olusturuldu."
